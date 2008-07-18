b8c6d17c5fcaf229266d545f931f9ad7ed2e4abc
jamie.fraser <jamie.fraser@acd71af0-6c51-0410-aad1-3302b24b684d>
2008-07-18 14:00:00 +0000

    Added unit test project

---
+++ trunk/eland.tests/UnitTests/WorldTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using eland.model;
+using eland.api;
+
+using MbUnit.Framework;
+
+namespace eland.tests.UnitTests
+{
+   [TestFixture]
+   public class WorldTests
+   {
+      [Test]
+      public void World_Create() 
+      {
+         World world = new World();
+         world.Height = 1000;
+         world.Width = 1000;
+         world.Name = "unit_test_world";
+
+         Repository<World> worldRep = new Repository<World>();
+         world = worldRep.Save(world);
+
+         Assert.AreNotEqual(Guid.Empty, world.Id);
+      }
+   }
+}
