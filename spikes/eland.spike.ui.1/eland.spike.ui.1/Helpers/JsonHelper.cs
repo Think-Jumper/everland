50b54edb48020828dff9a0d19ce00a8716d71ad9
jamie.fraser <jamie.fraser@acd71af0-6c51-0410-aad1-3302b24b684d>
2009-01-07 15:47:44 +0000

    Initial Checkin of a very simple UI spike

---
+++ spikes/eland.spike.ui.1/eland.spike.ui.1/Helpers/JsonHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace SilverlightSpike1.Helpers
+{
+    public class JsonHelper<T>
+    {
+        public static T ConvertJsonStringToObject(string json)
+        {
+            try
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    var bytes = Encoding.Unicode.GetBytes(json);
+                    memoryStream.Write(bytes, 0, bytes.Length);
+                    memoryStream.Position = 0;
+
+                    return ConvertJsonStringToObject(memoryStream);
+                }
+            }
+            catch (SerializationException)
+            {
+                return default(T);
+            }
+        }
+
+        public static T ConvertJsonStringToObject(Stream json)
+        {
+            try
+            {
+                var dataContractJsonSerializer = new DataContractJsonSerializer(typeof(T));
+                return (T)dataContractJsonSerializer.ReadObject(json);
+            }
+            catch (SerializationException)
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ spikes/eland.spike.ui.1/eland.spike.ui.1/Page.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using SilverlightSpike1.Helpers;
+
+namespace eland.spike.ui._1
+{
+    public partial class Page
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMain_Click(object sender, RoutedEventArgs e)
+        {
+            var searchURL = string.Format("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={0}", txtSearchQuery.Text.Trim());
+
+            var googleService = new WebClient();
+            googleService.OpenReadCompleted += googleService_OpenReadCompleted;
+            googleService.OpenReadAsync(new Uri(searchURL));
+        }
+
+        private void googleService_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            if (e.Error != null) return;
+
+            var result = e.Result;
+            var results = JsonHelper<GoogleResponseWrapper>.ConvertJsonStringToObject(result);
+
+            DisplaySearchResults(results.responseData);
+        }
+
+        public void DisplaySearchResults(GoogleSearchResults results)
+        {
+            grResults.SelectedIndex = -1;
+            grResults.ItemsSource = results.results;
+        }
+
+
+    }
+
+    public class GoogleResponseWrapper
+    {
+        public GoogleSearchResults responseData;
+        public string responseDetails;
+        public int responseStatus;
+    }
+
+    public class GoogleSearchResults
+    {
+        public GoogleResult[] results;
+    }
+
+    public class GoogleResult
+    {
+        public string GsearchResultClass { get; set; }
+        public string unescapedUrl { get; set; }
+        public string url { get; set; }
+        public string visibleUrl { get; set; }
+        public string cacheUrl { get; set; }
+        public string title { get; set; }
+        public string titleNoFormatting { get; set; }
+        public string content { get; set; }
+    }
+}
