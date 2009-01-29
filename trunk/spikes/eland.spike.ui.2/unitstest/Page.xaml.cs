a6dc8b2f72b6c707cbca8b0c2aee23bff6f26b05
jamie.fraser <jamie.fraser@acd71af0-6c51-0410-aad1-3302b24b684d>
2009-01-29 16:30:13 +0000

    More changes to GridManager behaviour Need to do a lot of refactoring

--- trunk/spikes/eland.spike.ui.2/unitstest/Page.xaml.cs
+++ trunk/spikes/eland.spike.ui.2/unitstest/Page.xaml.cs
@@ -29,10 +29,13 @@ namespace unitstest
             mouseDown = true;
         }
 
-   private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (selectedRectangle == null || LayoutRoot.Resources.Contains("stb_move"))
-                return;
+            var xPos = (int)e.GetPosition(cnvMain).X;
+            var yPos = (int)e.GetPosition(cnvMain).Y;
+
+            if (selectedRectangle == null || !LayoutRoot.Resources.Contains("stb_move"))
+                gMan.HighlightGridSquare(cnvMain, xPos, yPos);
 
             if (mouseDown)
             {
@@ -42,18 +45,15 @@ namespace unitstest
 
             var speed = slSpeed.Value;
 
-            var xPos = e.GetPosition(cnvMain).X;
-            var yPos = e.GetPosition(cnvMain).Y;
-
-            xPos = ((Math.Round(xPos / 10)) * 10) + stroke_width*2;
-            yPos = ((Math.Round(yPos / 10)) * 10) + stroke_width*2;
+            xPos = ((Math.Round(xPos / 10)) * 10) + stroke_width * 2;
+            yPos = ((Math.Round(yPos / 10)) * 10) + stroke_width * 2;
 
             var stb = new Storyboard();
             LayoutRoot.Resources.Add("stb_move", stb);
 
             var daX = new DoubleAnimation
                           {
-                              From = ((double) selectedRectangle.GetValue(Canvas.LeftProperty)),
+                              From = ((double)selectedRectangle.GetValue(Canvas.LeftProperty)),
                               To = xPos,
                               Duration = new Duration(TimeSpan.FromSeconds(speed)),
                               By = 10
@@ -77,7 +77,6 @@ namespace unitstest
 
             stb.Completed += Animation_Completed;
             stb.Begin();
-
         }
 
         private void Animation_Completed(object sender, EventArgs e)
@@ -87,12 +86,11 @@ namespace unitstest
 
         private void cnvMain_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            var gm = new GridManager();
-            gm.Draw(cnvMain, 10, 0.5);
+            gMan.Draw(cnvMain, 10, 0.5);
 
         }
 
-   }
+    }
 
     public class GridSquare
     {
@@ -110,12 +108,23 @@ namespace unitstest
 
         public GridSquare(int X, int Y, int Size)
         {
-            X1 = (Size*X);
-            X2 = (Size*(X + 1));
-            Y1 = (Size*Y);
-            Y2 = (Size*(Y + 1));
+            X1 = (Size * X);
+            X2 = (Size * (X + 1));
+            Y1 = (Size * Y);
+            Y2 = (Size * (Y + 1));
         }
-        
+
+        public void HighLight(Canvas surface)
+        {
+            var highlight = new Rectangle {Fill = new SolidColorBrush(Colors.Green)};
+            highlight.SetValue(Canvas.TopProperty, (double)Y1);
+            highlight.SetValue(Canvas.LeftProperty, (double)X1);
+            highlight.Width = X2 - X1;
+            highlight.Height = Y2 - Y1;
+
+            surface.Children.Add(highlight);
+        }
+
     }
 
     public class GridManager
@@ -139,12 +148,22 @@ namespace unitstest
                 for (var y = 0; y < numSquaresY; y++)
                 {
                     var g = new GridSquare(x, y, gridSize);
-                          
+
                     grid.Add(g);
                 }
             }
 
             DrawGridLines(surface, strokeWidth);
+        }
+
+        public void HighlightGridSquare(Canvas surface, int X, int Y)
+        {
+            foreach (var g in grid)
+            {
+                if (g.Intersects(new Point(X, Y)))
+                    g.HighLight(surface);
+
+            }
         }
 
         private void DrawGridLines(Canvas surface, double StrokeWidth)
@@ -161,6 +180,6 @@ namespace unitstest
                 surface.Children.Add(l);
             }
         }
-        
+
     }
 }
