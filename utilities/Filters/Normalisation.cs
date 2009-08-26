0b477a4f2f8fe35071ab9ceee682fd4438de9c46
jamie.fraser <jamie.fraser@acd71af0-6c51-0410-aad1-3302b24b684d>
2009-08-26 15:32:20 +0000

    Added Perlin & Improved Perlin algorithms for generating terrain.

--- utilities/Filters/Normalisation.cs
+++ utilities/Filters/Normalisation.cs
@@ -15,7 +15,17 @@ namespace eland.utilities
             return new NormalisationValues {a = min, b = max, d = values.Max(), c = values.Min()};
         }
 
+        public static NormalisationValues CalculateInitialValues(double[] values, float min, float max)
+        {
+            return new NormalisationValues { a = min, b = max, d = (float)values.Max(), c = (float)values.Min() };
+        }
+
         public static float Normalise(NormalisationValues normalVals, float value)
+        {
+            return (value - normalVals.c) * ((normalVals.b - normalVals.a) / (normalVals.d - normalVals.c)) + normalVals.a;
+        }
+
+        public static double Normalise(NormalisationValues normalVals, double value)
         {
             return (value - normalVals.c) * ((normalVals.b - normalVals.a) / (normalVals.d - normalVals.c)) + normalVals.a;
         }
